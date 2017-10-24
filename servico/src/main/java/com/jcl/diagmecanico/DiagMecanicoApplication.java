a6054b7c2b697b5c60e02ddb437bf12742f9baba
christian-tavares <christian.tavares@basis.com.br>
2017-10-24 15:30:38 -0200

    base

---
+++ servico/src/main/java/com/jcl/diagmecanico/DiagMecanicoApplication.java
@@ -0,0 +1,12 @@
+package com.jcl.diagmecanico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagMecanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagMecanicoApplication.class, args);
+	}
+}
